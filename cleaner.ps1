73a8f0aa9ce2b8dee0ce23e0e0d6ff07166aeb0b
Paweł Horoś <pawel.horos@gmail.com>
2016-11-18 13:09:04 +0100

    Update cleaner.ps1

--- cleaner.ps1
+++ cleaner.ps1
@@ -1,2 +1,2 @@
 ﻿$filename = $($args[0])
-(Get-Content -literalPath $filename).replace('<i>','').replace('</i>','').replace('<b>','').replace('</b>','').replace('{y:b}','') -replace'(\{[\s\S]*)}',''| Set-Content -literalPath $filename
+(Get-Content -literalPath $filename).replace('<i>','').replace('</i>','').replace('<b>','').replace('</b>','') -replace'(\{[\s\S]*)}',''| Set-Content -literalPath $filename
