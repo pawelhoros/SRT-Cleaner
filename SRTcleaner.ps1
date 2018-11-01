0de7e2e95828cbe42adfe335ae8aeb81c0ec0496
Paweł Horoś <pawel.horos@gmail.com>
2018-11-01 21:27:06 +0100

    File encoding auto detection
    Fixes #2

--- SRTcleaner.ps1
+++ SRTcleaner.ps1
@@ -1,3 +1,9 @@
 ForEach($filename in $args) {
-  (Get-Content -literalPath $filename).replace('<i>','').replace('</i>','').replace('<b>','').replace('</b>','') -replace'(\{[\s\S]*)}',''| Set-Content -literalPath $filename
+  # detect file encoding
+  [byte[]]$byte = get-content -Encoding byte -ReadCount 4 -TotalCount 4 -Path $filename
+  if ( $byte[0] -eq 0xef -and $byte[1] -eq 0xbb -and $byte[2] -eq 0xbf ) {  # UTF-8
+    (Get-Content -literalPath $filename).replace('<i>','').replace('</i>','').replace('<b>','').replace('</b>','').replace('♪','') -replace'(\{[\s\S]*)}',''| Set-Content -Encoding UTF8 -literalPath $filename
+  } else { # default = ANSI
+    (Get-Content -literalPath $filename).replace('<i>','').replace('</i>','').replace('<b>','').replace('</b>','').replace('♪','') -replace'(\{[\s\S]*)}',''| Set-Content -literalPath $filename
+  }
 }
